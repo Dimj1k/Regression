fcb41a8c93eb8bb7ea48f8d909a017feab67971a
Dimj <93049313+Dimj1k@users.noreply.github.com>
2023-05-24 17:36:15 +0300

    Delete src/abstracts directory

--- src/abstracts/__init__.py
+++
@@ -1,7 +0,0 @@
-from .ABCGraph import Graph
-from .ABCReadData import AbstractData
-from .ABCRegression import (
-    Regression,
-    AbstractOneDRegression,
-    AbstractMultiplyDRegression
-)
--- src/abstracts/src/abstracts/__init__.py
+++
@@ -1,6 +0,0 @@
-from .ABCGraph import Graph
-from .ABCReadData import AbstractData
-from .ABCRegression import (
-    AbstractOneDRegression,
-    AbstractMultiplyDRegression
-)
